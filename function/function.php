213cf7d3d335c2101b219aa532828b2d8026bada
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2022-03-01 20:59:18 +0700

    :airplane: update function
    Mengupdate function menjadi curl

--- function/function.php
+++ function/function.php
@@ -1,9 +1,23 @@
 <?php
 
+// function getQuote($link)
+// {
+//     $url = $link;
+//     $data = file_get_contents($url);
+//     $json = json_decode($data, true);
+//     return $json;
+// }
+
+// CURL function for API call to get quote with try catch
 function getQuote($link)
 {
     $url = $link;
-    $data = file_get_contents($url);
-    $json = json_decode($data, true);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($result, true);
     return $json;
 }
